b94d79cb384dbd158dfb70826727e1a0a6047a87
Yehor Shapanov <yehor.shapanov@gmail.com>
2019-09-30 21:26:30 +0300

    Added grid_path
    Added solution to grid_path
    Changed two_sets

--- grid_paths.cpp
+++ grid_paths.cpp
@@ -13,10 +13,56 @@
 #include <cmath>
 #include <algorithm>
 #include <climits>
+#include <fstream>
 using namespace std;
 typedef long long ll;
 
+
+int recoursive_subroutine(vector<vector<int>>& field, string& query, int y, int x, int i, char m) {
+  if (i == 48) return 1;
+  if (field[6][0]) return 0;
+  if (m == 'L' && (x == 0 || field[y][x-1]) && y > 0 && y < 6 && !field[y-1][x] && !field[y+1][x]) return 0;
+  if (m == 'R' && (x == 6 || field[y][x+1]) && y > 0 && y < 6 && !field[y-1][x] && !field[y+1][x]) return 0;
+  if (m == 'U' && (y == 0 || field[y-1][x]) && x > 0 && x < 6 && !field[y][x-1] && !field[y][x+1]) return 0;
+  if (m == 'D' && (y == 6 || field[y+1][x]) && x > 0 && x < 6 && !field[y][x-1] && !field[y][x+1]) return 0;
+  if (query[i] == '?') {
+    int r[] = {-1, 0, 1, 0};
+    int c[] = {0, -1, 0, 1};
+    int k = 0;
+    for (int j = 0; j < 4; j++) {
+      int yy = y + r[j];
+      int xx = x + c[j];
+      if (yy < 0 || yy > 6) continue;
+      if (xx < 0 || xx > 6) continue;
+      if (field[yy][xx]) continue;
+      field[yy][xx] = true;
+      k += recoursive_subroutine(field, query, yy, xx, i + 1, "ULDR"[j]);
+      field[yy][xx] = false;
+    }
+    return k;
+  }
+  if (query[i] == 'L') x--;
+  else if (query[i] == 'R') x++;
+  else if (query[i] == 'U') y--;
+  else if (query[i] == 'D') y++;
+  if (x < 0 || x > 6) return 0;
+  if (y < 0 || y > 6) return 0;
+  if (field[y][x]) return 0;
+  field[y][x] = true;
+  int k = recoursive_subroutine(field, query, y, x, i + 1, query[i]);
+  field[y][x] = false;
+  return k;
+}
+
 int main() {
-    
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    string s;
+    cin >> s;
+    vector<vector<int>> field(7, vector<int>(7));
+    field[0][0] = 1;
+    cout << recoursive_subroutine(field, s, 0, 0, 0, 0) << endl;
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::minutes>(end - begin).count() << "[min]" << std::endl;
     return 0;
 }
--- two_sets.cpp
+++ two_sets.cpp
@@ -5,63 +5,39 @@
 //  Created by Yehor Shapanov on 9/23/19.
 //  Copyright © 2019 Yehor Shapanov. All rights reserved.
 #include "stdc++.h"
-int main() {
+int main() {    
     int n;
     cin >> n;
-    int left = 0, right = 0;
-    queue<int> left_queue, right_queue;
-    for (int i = 0; i < n / 2; i++) {
-        if (n % 2) {
-            if (i % 2) {
-                left += i + 1;
-                right += n - i;
-                left_queue.push(i + 1);
-                right_queue.push(n - i);
-            } else {
-                right += i + 1;
-                left += n - i;
-                left_queue.push(n - i);
-                right_queue.push(i + 1);
-            }
-        } else {
-            if (i % 2) {
-                right += i + 1;
-                right += n - i;
-                right_queue.push(n - i);
-                right_queue.push(i + 1);
-            } else {
-                left += i + 1;
-                left += n - i;
-                left_queue.push(i + 1);
-                left_queue.push(n - i);
-            }
+    int t = n * (n + 1) / 2;
+    if (t % 2) cout << "NO\n";
+    else {
+      t /= 2;
+      vector<int> a;
+      int s = 0;
+      int e = 0;
+      int i = n;
+      while (true) {
+        a.push_back(i);
+        s += i--;
+        e = t - s;
+        if (!e) break;
+        if (e <= i) {
+          a.push_back(e);
+          break;
         }
+      }
+      cout << "YES\n" << a.size() << endl;
+      for (int j = 0; j < a.size(); j++) {
+        cout << a[j];
+        if (j < a.size() - 1) cout << " ";
+      }
+      cout << endl;
+      cout << n - a.size() << endl;
+      for (int j = i; j > 0; j--) {
+        if (j == e) continue;
+        cout << j;
+        if ((e != 1 && j > 1) || (e == 1 && j > 2)) cout << " ";
+      }
+      cout << endl;
     }
-    
-    if (n % 2 == 0) {
-        if (left == right) {
-            cout << "YES" << endl;
-        } else {
-            cout << "NO";
-            return 0;
-        }
-    } else {
-        int el = n / 2 + 1;
-        if (abs(left - right) == el) {
-            cout << "YES" << endl;
-            if (left > right) {
-                right_queue.push(el);
-            } else {
-                left_queue.push(el);
-            }
-        } else {
-            cout << "NO";
-            return 0;
-        }
-    }
-    cout << left_queue.size() << endl;
-    while (!left_queue.empty()) { cout << left_queue.front() << " "; left_queue.pop(); }
-    cout << endl << right_queue.size() << endl;
-    while (!right_queue.empty()) { cout << right_queue.front() << " "; right_queue.pop(); }
-    return 0;
 }
